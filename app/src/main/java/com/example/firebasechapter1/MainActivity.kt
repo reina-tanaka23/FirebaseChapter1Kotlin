0b787eced4aff0a7dd0ef346c5222a63cefbde5b
reina.tanaka <reina.tanaka0223@gmail.com>
2021-12-19 21:42:51 +0900

    FirebaseChapter2 facebook ログイン認証

--- app/src/main/java/com/example/firebasechapter1/MainActivity.kt
+++ app/src/main/java/com/example/firebasechapter1/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.firebasechapter1
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.firebasechapter1.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,25 +25,68 @@ class MainActivity : AppCompatActivity() {
 
     private var fbase: FirebaseApp? = null
 
+    private var dataText: TextView? = null
+    private val db: FirebaseFirestore? = null
+    private var people: CollectionReference? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        dataText = findViewById(R.id.dataText);
+
         setSupportActionBar(binding.toolbar)
 
         fbase = FirebaseApp.initializeApp(this)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
             val msg: String = "Firebase: " + fbase!!.name
             Snackbar.make(view, msg, Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        val db = Firebase.firestore
+        people = db.collection("people")
+
+
+//        people!!.get().addOnCompleteListener { task ->
+//            if (task.isSuccessful) {
+//                var result = ""
+//                for (document in task.result!!) {
+//                    val data = document.data
+//                    result += (data["name"].toString() + " ["
+//                            + data["mail"].toString() + ":"
+//                            + data["age"].toString() + "]\n")
+//                }
+//                dataText!!.setText(result)
+//            } else {
+//                dataText!!.setText("can't load data...")
+//            }
+//        }
+
+        val docRef = db.collection("people")
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d("DEBUG", "DocumentSnapshot data: ${document}")
+                } else {
+                    Log.d("DEBUG", "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("DEBUG", "get failed with ", exception)
+            }
+    }
+
+//    fun doAction(view: View?) {
+//        val fstr: String = nameText.getText().toString() + ""
+//    }
+
+
+    override fun onStart() {
+        super.onStart()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -55,11 +103,5 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
